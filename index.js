f67e0f862667e89269a3716e5150963bca86b682
Pipsy <gpflaslik7112@gmail.com>
2026-03-23 23:19:48 +0300

    change of code details

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ function findMaximum(num1, num2) {
   return Math.max(num1, num2);
 }
 
-function isPalindrome(word) {
-  let lowerCaseWord = word.toLowerCase();
-  
- 
-
-  let reversedWord = lowerCaseWord.split('').reverse().join('');
-  return lowerCaseWord === reversedWord;
-}
 
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
   let discountAmount = originalPrice * (discountPercentage / 100);
@@ -31,6 +23,6 @@ function calculateDiscountedPrice(originalPrice, discountPercentage) {
 console.log("Tax on $50:", calculateTax(50)); 
 console.log("Uppercase:", convertToUpperCase("javascript is powerful")); 
 console.log("Max number:", findMaximum(150, 42)); 
-console.log("Is 'level' a palindrome?:", isPalindrome("level")); 
-console.log("Discounted Price:", calculateDiscountedPrice(100, 20)); 
+ 
+ 
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };

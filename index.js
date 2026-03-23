1617c47d0d0097520e21f367f688cdbdf2ae3f48
Pipsy <gpflaslik7112@gmail.com>
2026-03-23 23:17:11 +0300

    initial commit of js file

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
 
+function findMaximum(num1, num2) {
 
-// This is required for the test to function properly  
+  return Math.max(num1, num2);
+}
+
+function isPalindrome(word) {
+  let lowerCaseWord = word.toLowerCase();
+  
+ 
+
+  let reversedWord = lowerCaseWord.split('').reverse().join('');
+  return lowerCaseWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discountAmount = originalPrice * (discountPercentage / 100);
+  
+   return originalPrice - discountAmount;
+}
+
+console.log("Tax on $50:", calculateTax(50)); 
+console.log("Uppercase:", convertToUpperCase("javascript is powerful")); 
+console.log("Max number:", findMaximum(150, 42)); 
+console.log("Is 'level' a palindrome?:", isPalindrome("level")); 
+console.log("Discounted Price:", calculateDiscountedPrice(100, 20)); 
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
